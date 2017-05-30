aa27911e7cc20ecb2d0e0817785c4c65e3d53f6d
Laurel Christ <laurel.g.christ@gmail.com>
2017-05-29 22:21:35 -0500

    Condensing output

--- Driver.java
+++ Driver.java
@@ -19,25 +19,35 @@ public class Driver
 		//testing INT min heap
 		//
 		IntMinHeap test1 = new IntMinHeap(10);
-		Random random = new Random();
+		Random random = new Random(1);
 
 		for(int i = 0; i < 10; i++){
 			test1.insert(random.nextInt(100));
 		}
-		System.out.println("Min Heap: " + test1.toString());
-
-		System.out.println("Get root: " + test1.getRoot());
+				System.out.println("Min Heap: " + test1.toString());
 		
-		for(int i = 0; i < 10; i++)
-		{
-			System.out.println("Extract min: " + test1.getRoot());
-			test1.extractMin();
-		}
+				System.out.println("Get root: " + test1.getRoot());
+				
+				System.out.println("\nBefore decrease key: " + test1.toString());
+				
+				test1.decreaseKey(8, 1); 
+				
+				System.out.println("After decrease key: " + test1.toString());
+				
+				for(int i = 0; i < 10; i++)
+				{
+					System.out.println("Extract min: " + test1.getRoot());
+					test1.extractMin();
+				}
+
+
 
 		//
 		//Test NODE MIN HEAP
 		//
 		NodeMinHeap<Node> test2 = new NodeMinHeap<Node>(9);
+
+		System.out.println("test node min heap: \n");
 
 		//add 10 nodes
 		for(int i = 1; i < 2; i++)
@@ -56,22 +66,22 @@ public class Driver
 			Node I = new Node("I", 9);
 
 			//Edge(int from, int to, int theWeight)
-			A.addEdge(new Edge(A.getId(), B.getId(), 7));
-			A.addEdge(new Edge(A.getId(), E.getId(), 6));
-			A.addEdge(new Edge(A.getId(), G.getId(), 8));
-			
-			B.addEdge(new Edge(B.getId(), C.getId(), 19));
+			A.addEdge(new Edge(A.getId(), B.getId(), 1));
+			A.addEdge(new Edge(A.getId(), E.getId(), 2));
+			A.addEdge(new Edge(A.getId(), G.getId(), 3));
+
+			B.addEdge(new Edge(B.getId(), C.getId(), 4));
 			D.addEdge(new Edge(D.getId(), C.getId(), 5));
-			D.addEdge(new Edge(D.getId(), I.getId(), 3));
-			E.addEdge(new Edge(E.getId(), C.getId(), 21));
-			E.addEdge(new Edge(E.getId(), F.getId(), 13));
-			E.addEdge(new Edge(E.getId(), H.getId(), 16));
-			F.addEdge(new Edge(F.getId(), D.getId(), 2));
-			G.addEdge(new Edge(G.getId(), H.getId(), 8));
-			H.addEdge(new Edge(H.getId(), F.getId(), 2));
-			I.addEdge(new Edge(I.getId(), F.getId(), 4));
-			I.addEdge(new Edge(I.getId(), H.getId(), 3));
-			
+			D.addEdge(new Edge(D.getId(), I.getId(), 6));
+			E.addEdge(new Edge(E.getId(), C.getId(), 7));
+			E.addEdge(new Edge(E.getId(), F.getId(), 8));
+			E.addEdge(new Edge(E.getId(), H.getId(), 9));
+			F.addEdge(new Edge(F.getId(), D.getId(), 10));
+			G.addEdge(new Edge(G.getId(), H.getId(), 11));
+			H.addEdge(new Edge(H.getId(), F.getId(), 12));
+			I.addEdge(new Edge(I.getId(), F.getId(), 13));
+			I.addEdge(new Edge(I.getId(), H.getId(), 14));
+
 			test2.insert(A);
 			test2.insert(B);
 			test2.insert(C);
@@ -81,15 +91,27 @@ public class Driver
 			test2.insert(G);
 			test2.insert(H);
 			test2.insert(I);
+
+			for(int j = 1; j < 10; j++)
+			{
+				test2.getNode(j).setLengthOfPath(j*2);
+			}
 		}	
 
-		System.out.println(test2.toString());
 
-		for(int i = 0; i < 8; i++)
+		System.out.println("MinHeap: \n" + test2.toString());
+
+		test2.getNode(8).setLengthOfPath(3);
+		test2.decreaseKey(8);
+
+		System.out.println("MinHeap: \n" + test2.toString());
+
+		for(int i = 0; i < 10; i++)
 		{
-			//test2.extractMin();
-			System.out.println(test2.extractMin().getData());
+			System.out.println(test2.extractMin());
 		}
+
+		System.out.println("MinHeap: \n" + test2.toString());
 
 	}
 }
--- HypergraphMain.java
+++ HypergraphMain.java
@@ -22,134 +22,150 @@ public class HypergraphMain
 	public static void main(String[] args)
 	{
 
-        Hypergraph hGraph = new Hypergraph();
-        
-        ArrayList aryList = new ArrayList();
-       
-        aryList.add(0);
-        aryList.add(5);
-        
-        //
-        //Create hgraph 
-        //
-        for(int count = 0; count < 10; count++)
-        {
-        	//add six nodes 
-        	//addNode(T data, int id)
-            hGraph.addNode(count, count);
-            //populate array list
-            aryList.add(count);
-        }
-        
-        ArrayList<Integer> fromList = new ArrayList<Integer>();
-        fromList.add(0);
-        //(0 -> 1)
-        hGraph.addEdge(fromList, 1);
-        
-        //(0 -> 2)
-        hGraph.addEdge(fromList, 2);
-        
-        ArrayList<Integer> fromList2 = new ArrayList<Integer>();
-        fromList2.add(1);
-        fromList2.add(2);
-        fromList2.add(5);
-        //(1,2,5 -> 3)
-        hGraph.addEdge(fromList2, 3);
-        //(1,2,5 - 4)
-        hGraph.addEdge(fromList2, 4);
-        
-        //print the hypergraph 
+		Hypergraph hGraph = new Hypergraph();
+
+		ArrayList aryList = new ArrayList();
+
+		aryList.add(0);
+		aryList.add(5);
+
+		//
+		//Create hgraph 
+		//
+		for(int count = 0; count < 6; count++)
+		{
+			//add six nodes 
+			//addNode(T data, int id)
+			hGraph.addNode(count, count);
+			//populate array list
+			aryList.add(count);
+		}
+
+		ArrayList<Integer> fromList = new ArrayList<Integer>();
+		fromList.add(0);
+		//(0 -> 1)
+		hGraph.addEdge(fromList, 1);
+
+		//(0 -> 2)
+		hGraph.addEdge(fromList, 2);
+
+		ArrayList<Integer> fromList2 = new ArrayList<Integer>();
+		fromList2.add(1);
+		fromList2.add(2);
+		fromList2.add(5);
+		//(1,2,5 -> 3)
+		hGraph.addEdge(fromList2, 3);
+		//(1,2,5 - 4)
+		hGraph.addEdge(fromList2, 4);
+
+		//print the hypergraph 
 		//System.out.println(hGraph);
-        
+
 		//
 		//make hypergraph --> digraph
 		//
-//        DiGraph DG = new DiGraph(hGraph);
-//        System.out.println("Digraph from Hypergraph: " + DG);
-//        
-//      System.out.println("Dijkstra");
-//		Dijkstra dijkstra = new Dijkstra(DG, DG.getNode(0));
-//		dijkstra.execute();
-//		System.out.println(dijkstra.toString());
-        
+		DiGraph DG = new DiGraph(hGraph);
+		//		System.out.println("Digraph from Hypergraph: \n" + DG);
+		//
+		//		System.out.println("Dijkstra");
+		//		Dijkstra dijkstra = new Dijkstra(DG, DG.getNode(1));
+		//		dijkstra.execute();
+		//		System.out.println(dijkstra.toString());
+
 		//
 		//generate random hypergraph		
-        //
-		HypergraphGenerator randHypergraph = new HypergraphGenerator();
-		
-		ArrayList<Hypernode> nodes = new ArrayList();
-		
-		for(int i = 0; i < 1500; i++)
-		{
-			nodes.add(new Hypernode(i,i));
-		}
-		
-		randHypergraph.genRandomHypergraph(nodes, 1);
-		
-		DiGraph DG3 = new DiGraph(randHypergraph.genRandomHypergraph(nodes, 1));
-		
-//		System.out.println("-----------------------------PRINT RAND HYPERGRAPH--------------------------------");
-//		System.out.println(randHypergraph.genRandomHypergraph(nodes, 1).toString());
-//		
-//		System.out.println("-----------------------------PRINT RAND DIGRAPH--------------------------------");
-//		System.out.println("Digraph: \n" + DG3);
-		
-		Dijkstra dijkstra1 = new Dijkstra(DG3, DG3.getNode(0));
-		dijkstra1.execute();
-		System.out.println(dijkstra1.toString());
-		
-//        //
-//        //create path!
-//        //
-//        ArrayList nodesToBePebbled = new ArrayList();
-//        nodesToBePebbled.add(0);
-//        nodesToBePebbled.add(5);
-//        
-//		PebblerHypergraph pGraph = new PebblerHypergraph();
-//		try
-//		{
-//			pGraph = hGraph.getPebbledHypergraph();
-//		}
-//
-//		catch(Exception e)
-//		{
-//			System.out.println("error");
-//		}
-//		
-//		System.out.println(pGraph);
-//		
-//		//
-//		//Create all paths w/just one step
-//		//
-//		Pebbler pebbler = new Pebbler(pGraph);
-//
-//		pebbler.pebble(nodesToBePebbled);
-//
-//		HyperedgeMultiMap map = pebbler.getForwardEdges();
-//		
-//		System.out.println("Map: \n" + map);
-//
-//		//
-//		//Create one path using multiple steps
-//		//
-//		PathGenerator pathGen = new PathGenerator(hGraph);
-//		PathHashMap pathMap = new PathHashMap(map, hGraph.size());
-//
-//		int GOAL_NODE = 4;
-//		pathGen.GeneratePathBackwardToLeaves(pathMap, map, GOAL_NODE);
-//
-//		// Write code to print all the paths (from the pathHashMap) with goal node 4.
-//		ArrayList pathList = pathMap.get(GOAL_NODE);
-//
-//		for (int i = 0; i < pathList.size(); i++)
-//		{
-//			System.out.println("Path list: " + pathList.get(i));
-//		}
-	
-		
+		//
+		//		HypergraphGenerator randHypergraph = new HypergraphGenerator();
+		//
+		//		ArrayList<Hypernode> nodes = new ArrayList();
+		//
+		//		for(int i = 0; i < 6; i++)
+		//		{
+		//			nodes.add(new Hypernode(i,i));
+		//		}
+		//
+		//		randHypergraph.genRandomHypergraph(nodes, 1);
+		//
+		//		DiGraph DG3 = new DiGraph(randHypergraph.genRandomHypergraph(nodes, 1));
+		//
+		//		System.out.println("-----------------------------PRINT RAND HYPERGRAPH--------------------------------");
+		//		System.out.println(randHypergraph.genRandomHypergraph(nodes, 1).toString());
+		//
+		//		System.out.println("-----------------------------PRINT RAND DIGRAPH--------------------------------");
+		//		System.out.println("Digraph: \n" + DG3);
+		//
+		//		Dijkstra dijkstra1 = new Dijkstra(DG3, DG3.getNode(0));
+		//		dijkstra1.execute();
+		//		System.out.println(dijkstra1.toString());
+
+
+
+		//		for (int i = 0; i < DG.getNumNode(); i++)
+		//		{
+		//			System.out.println("---------------------------------EVALUATE ALL NODES-----------------------------");
+		//			System.out.println("Node " + i);
+		Dijkstra dijkstra2 = new Dijkstra(DG, DG.getNode(0));
+		//			dijkstra2.execute();
+		//			System.out.println(dijkstra2.toString());
+		//			dijkstra2.resetLengthOfPath();
+		//		}
+		System.out.println("Execute ALL");
+		dijkstra2.excecuteAll();
+
+
+
+		//        //
+		//        //create path!
+		//        //
+		//        ArrayList nodesToBePebbled = new ArrayList();
+		//        nodesToBePebbled.add(0);
+		//        nodesToBePebbled.add(5);
+		//        
+		//		PebblerHypergraph pGraph = new PebblerHypergraph();
+		//		try
+		//		{
+		//			pGraph = hGraph.getPebbledHypergraph();
+		//		}
+		//
+		//		catch(Exception e)
+		//		{
+		//			System.out.println("error");
+		//		}
+		//		
+		//		System.out.println(pGraph);
+		//		
+		//		//
+		//		//Create all paths w/just one step
+		//		//
+		//		Pebbler pebbler = new Pebbler(pGraph);
+		//
+		//		pebbler.pebble(nodesToBePebbled);
+		//
+		//		HyperedgeMultiMap map = pebbler.getForwardEdges();
+		//		
+		//		System.out.println("Map: \n" + map);
+		//
+		//		//
+		//		//Create one path using multiple steps
+		//		//
+		//		PathGenerator pathGen = new PathGenerator(hGraph);
+		//		PathHashMap pathMap = new PathHashMap(map, hGraph.size());
+		//
+		//		int GOAL_NODE = 4;
+		//		pathGen.GeneratePathBackwardToLeaves(pathMap, map, GOAL_NODE);
+		//
+		//		// Write code to print all the paths (from the pathHashMap) with goal node 4.
+		//		ArrayList pathList = pathMap.get(GOAL_NODE);
+		//
+		//		for (int i = 0; i < pathList.size(); i++)
+		//		{
+		//			System.out.println("Path list: " + pathList.get(i));
+		//		}
+
+
 	}
 
-	
+
 
 }
 
